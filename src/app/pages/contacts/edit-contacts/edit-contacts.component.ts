f841279f9aff9a4af626ebdf6acdfdc593e5bd96
Maria Paula Parra <mariapparra@gmail.com>
2025-06-16 10:18:11 -0500

    mp

--- src/app/pages/contacts/edit-contacts/edit-contacts.component.ts
+++ src/app/pages/contacts/edit-contacts/edit-contacts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../../../services/api.service';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
